2c63e4482efb2380665526b74cf9eeaaa56fc1fc
Matthew Lawson <mlawson424@gmail.com>
2013-05-10 20:43:27 -0400

    add

--- Projects/first progam.py
+++ Projects/first progam.py
@@ -22,15 +22,42 @@ class MyFrame (wx.Frame):
         filemenu = wx.Menu()
 
         #wx.ID_ABOUT and wx.ID_EXIT are standard IDs
-        filemenu.Append(wx.ID_ABOUT, "&About","Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,"E&xit","Exit this program")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About","Information about this program")
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", "Open a file")
+        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit","Exit this program")
 
         #Creating the menubar
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "&File") #Adding Filemenu to the MenuBar
         self.SetMenuBar(menuBar) #Adding the menubar to the frame content
+
+        #Create Events
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+
         self.Show(True)
+
+    def OnAbout(self,e):
+        #A message with dialog box OK
+        dlg = wx.MessageDialog(self, "Created By Matt Lawson", "Small Text Editor", wx.OK)
+        dlg.ShowModal() #Show it
+        dlg.Destroy() #Destroy frame after finishing
+
+    def OnExit(self,e):
+        self.Close(True) #Close the frame
+
+    def OnOpen(self,e):
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
 
 app = wx.App(False) #Create a new app, don't redirect stdout/stderr to a window
 frame = MyFrame(None, "First Program") #A Frame is a top-level window
